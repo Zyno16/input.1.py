dbe07bcf32a2143a6c9fdf135131538e37e52816
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-23 19:19:15 +0000

    Add files via upload

---
+++ input.2.py
@@ -0,0 +1,11 @@
+num1 =int(input("enter the first number: "))
+num2 =int(input("enter the number 2: "))
+
+print("=====")
+print(type(num1))
+print("========")
+print(type(num2))
+print("===========")
+print("the sum is ", num1+num2)
+print("===========")
+input("Press enter to exit")
